54ecb11bcebb36224e15b82d8f9e1e8bc94b2014
Hammy <bamhamyt1@gmail.com>
2026-04-24 23:35:56 -0500

    init: Main.java beginning initiation for application

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,5 @@
-package main;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Java version: " + System.getProperty("java.version"));
+        System.out.println("Initializing KovaaK's Aim Trainer Stat Tracker...");
     }
 }
